6ea404a16cc56cc6fb024b6062776947c0d58b50
Isaac Whitfield <iwhitfield@appcelerator.com>
2016-04-04 02:40:23 +0100

    Bump to v0.8.2

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Cachex.Mixfile do
         },
         maintainers: [ "Isaac Whitfield" ]
       },
-      version: "0.8.1",
+      version: "0.8.2",
       elixir: "~> 1.2",
       deps: deps,
       docs: [
