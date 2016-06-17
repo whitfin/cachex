90d7b1734159f732fc0ed4d63fa8d0dcea31d010
Isaac Whitfield <iwhitfield@appcelerator.com>
2016-06-17 23:07:59 +0100

    Update dependencies and mix.lock

--- mix.exs
+++ mix.exs
@@ -66,9 +66,9 @@ defmodule Cachex.Mixfile do
       # Local dependencies
       { :benchfella,   "~> 0.3.2",  optional: true, only: [ :dev, :test ] },
       { :benchwarmer,  "~> 0.0.2",  optional: true, only: [ :dev, :test ] },
-      { :credo,        "~> 0.4.2",  optional: true, only: [ :dev, :test ] },
+      { :credo,        "~> 0.4.3",  optional: true, only: [ :dev, :test ] },
       { :earmark,      "~> 0.2.1",  optional: true, only: [ :dev, :test ] },
-      { :ex_doc,       "~> 0.11.5", optional: true, only: [ :dev, :test ] },
+      { :ex_doc,       "~> 0.12.0", optional: true, only: [ :dev, :test ] },
       { :excoveralls,  "~> 0.5.4",  optional: true, only: [ :dev, :test ] },
       { :exprof,       "~> 0.2.0",  optional: true, only: [ :dev, :test ] },
       { :power_assert, "~> 0.0.8",  optional: true, only: [ :dev, :test ] }
