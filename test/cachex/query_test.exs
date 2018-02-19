b8b85fe480c19b86b854c41e84b190a28bd75f1b
Isaac Whitfield <iw@whitfin.io>
2018-02-18 21:48:18 -0800

    Improve some test coverage/resilience

--- test/cachex/query_test.exs
+++ test/cachex/query_test.exs
@@ -10,38 +10,38 @@ defmodule Cachex.QueryTest do
     query2 = Cachex.Query.create_query(true, :key)
 
     # verify the form of the first query
-    assert query1 == [
+    assert [
       {
         { :_, :"$1", :"$2", :"$3", :"$4" },
         [
           { :andalso,
             { :orelse,
               { :==, :"$3", nil },
-              { :>, {:+, :"$2", :"$3" }, now() }
+              { :>, {:+, :"$2", :"$3" }, _now }
             },
             true
           }
         ],
         [ :"$_" ]
       }
-    ]
+    ] = query1
 
     # verify the form of the second query
-    assert query2 == [
+    assert [
       {
         { :_, :"$1", :"$2", :"$3", :"$4" },
         [
           { :andalso,
             { :orelse,
               { :==, :"$3", nil },
-              { :>, {:+, :"$2", :"$3" }, now() }
+              { :>, {:+, :"$2", :"$3" }, _now }
             },
             true
           }
         ],
         [ :"$1" ]
       }
-    ]
+    ] = query2
   end
 
   # The `create_expired_query()` function is just a wrapper to `create_query`
@@ -52,36 +52,36 @@ defmodule Cachex.QueryTest do
     query2 = Cachex.Query.create_expired_query(:key)
 
     # verify the form of the first query
-    assert query1 == [
+    assert [
       {
         { :_, :"$1", :"$2", :"$3", :"$4" },
         [
           { :not,
             { :orelse,
               { :==, :"$3", nil },
-              { :>, {:+, :"$2", :"$3" }, now() }
+              { :>, {:+, :"$2", :"$3" }, _now }
             }
           }
         ],
         [ :"$_" ]
       }
-    ]
+    ] = query1
 
     # verify the form of the second query
-    assert query2 == [
+    assert [
       {
         { :_, :"$1", :"$2", :"$3", :"$4" },
         [
           { :not,
             { :orelse,
               { :==, :"$3", nil },
-              { :>, {:+, :"$2", :"$3" }, now() }
+              { :>, {:+, :"$2", :"$3" }, _now }
             }
           }
         ],
         [ :"$1" ]
       }
-    ]
+    ] = query2
   end
 
   # The `create_unexpired_query()` function is just a wrapper to `create_query`
@@ -92,15 +92,47 @@ defmodule Cachex.QueryTest do
     query2 = Cachex.Query.create_unexpired_query(:key)
 
     # verify the form of the first query
-    assert query1 == [
+    assert [
       {
         { :_, :"$1", :"$2", :"$3", :"$4" },
         [
           { :orelse,
             { :==, :"$3", nil },
-            { :>, {:+, :"$2", :"$3" }, now() }
+            { :>, {:+, :"$2", :"$3" }, _now }
           }
         ],
+        [ :"$_" ]
+      }
+    ] = query1
+
+    # verify the form of the second query
+    assert [
+      {
+        { :_, :"$1", :"$2", :"$3", :"$4" },
+        [
+          { :orelse,
+            { :==, :"$3", nil },
+            { :>, {:+, :"$2", :"$3" }, _now }
+          }
+        ],
+        [ :"$1" ]
+      }
+    ] = query2
+  end
+
+  # This test just covers the default value checking when creating raw
+  # queries with none of the provided bindings. This does nothing more
+  # than validate structure, as there are no attached conditions added.
+  test "creating raw queries" do
+    # create a query with a true filter
+    query1 = Cachex.Query.create_raw_query(true)
+    query2 = Cachex.Query.create_raw_query(true, :key)
+
+    # verify the form of the first query
+    assert query1 == [
+      {
+        { :_, :"$1", :"$2", :"$3", :"$4" },
+        [ true ],
         [ :"$_" ]
       }
     ]
@@ -109,12 +141,7 @@ defmodule Cachex.QueryTest do
     assert query2 == [
       {
         { :_, :"$1", :"$2", :"$3", :"$4" },
-        [
-          { :orelse,
-            { :==, :"$3", nil },
-            { :>, {:+, :"$2", :"$3" }, now() }
-          }
-        ],
+        [ true ],
         [ :"$1" ]
       }
     ]
