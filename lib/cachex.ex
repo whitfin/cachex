d217eefc9f8266f669598b92e8530f4fc15ebf88
Isaac Whitfield <iw@whitfin.io>
2018-03-29 22:49:48 -0700

    Avoid warnings for @deprecated on < 1.6

--- lib/cachex.ex
+++ lib/cachex.ex
@@ -1137,14 +1137,16 @@ defmodule Cachex do
   @doc """
   Deprecated implementation delegate of `put/4`.
   """
-  @deprecated "Please migrate to using put/4 instead"
+  if Version.match?(System.version(), ">= 1.6.0"),
+    do: @deprecated "Please migrate to using put/4 instead"
   def set(cache, key, value, options \\ []),
     do: put(cache, key, value, options)
 
   @doc """
   Deprecated implementation delegate of `put_many/3`.
   """
-  @deprecated "Please migrate to using put_many/3 instead"
+  if Version.match?(System.version(), ">= 1.6.0"),
+    do: @deprecated "Please migrate to using put_many/3 instead"
   def set_many(cache, pairs, options \\ []),
     do: put_many(cache, pairs, options)
 
