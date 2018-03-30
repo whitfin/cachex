21d4bd2183c42cabe3475b194cf65651f12900d0
Isaac Whitfield <iw@whitfin.io>
2018-03-29 23:00:59 -0700

    Bump to v3.0.2

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Cachex.Mixfile do
         },
         maintainers: [ "Isaac Whitfield" ]
       },
-      version: "3.0.1",
+      version: "3.0.2",
       elixir: "~> 1.2",
       deps: deps(),
       docs: [
