d1481049462c2d55bcdb450b893df964e9d12e23
Isaac Whitfield <iw@whitfin.io>
2022-07-10 09:27:11 -0700

    Add formatting checks to CI

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,6 @@
 [
   # set of input files and directories to apply formatting to
-  inputs: ["{mix,.credo,.formatter}.exs", "{benchmarks,test}/**/*.{ex,exs}"],
+  inputs: ["{mix,.credo,.formatter}.exs", "{benchmarks,lib,test}/**/*.{ex,exs}"],
 
   # match the Credo linter
   line_length: 80
