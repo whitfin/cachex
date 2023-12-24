2a762ef2baa3138708e5c7ea8f37baa0b54cba7b
Isaac Whitfield <iw@whitfin.io>
2023-12-24 12:01:19 +0000

    Fix false linting warning

--- .credo.exs
+++ .credo.exs
@@ -55,7 +55,7 @@
         {Credo.Check.Readability.ModuleDoc},
         {Credo.Check.Readability.ModuleNames},
         {Credo.Check.Readability.ParenthesesInCondition},
-        {Credo.Check.Readability.PredicateFunctionNames},
+        {Credo.Check.Readability.PredicateFunctionNames, false},
         {Credo.Check.Readability.PreferImplicitTry},
         {Credo.Check.Readability.RedundantBlankLines},
         {Credo.Check.Readability.Specs, false},
