af9576ee2001a5d7df5e4dbe3ffe453f43a6a93c
Isaac Whitfield <iw@whitfin.io>
2024-09-05 17:30:21 +0100

    Fix naming changes caused during refactor

--- lib/cachex/query.ex
+++ lib/cachex/query.ex
@@ -32,14 +32,14 @@ defmodule Cachex.Query do
   """
   @spec expired(any) :: [{tuple, [tuple], [any]}]
   def expired(output \\ :entry),
-    do: raw(expired_map_clauses(), output)
+    do: raw(expired_clause(), output)
 
   @doc """
   Creates a match condition for expired records.
   """
-  @spec expired_map_clauses :: tuple
-  def expired_map_clauses,
-    do: {:not, unexpired_map_clauses()}
+  @spec expired_clause :: tuple
+  def expired_clause,
+    do: {:not, unexpired_clause()}
 
   @doc """
   Creates a raw query, ignoring expiration.
@@ -60,13 +60,13 @@ defmodule Cachex.Query do
   """
   @spec unexpired(any) :: [{tuple, [tuple], [any]}]
   def unexpired(output \\ :entry),
-    do: raw(unexpired_map_clauses(), output)
+    do: raw(unexpired_clause(), output)
 
   @doc """
   Creates a match condition for unexpired records.
   """
-  @spec unexpired_map_clauses :: tuple
-  def unexpired_map_clauses,
+  @spec unexpired_clause :: tuple
+  def unexpired_clause,
     do:
       {:orelse, {:==, map_clauses(:ttl), nil},
        {:>, {:+, map_clauses(:touched), map_clauses(:ttl)}, now()}}
@@ -76,16 +76,16 @@ defmodule Cachex.Query do
   """
   @spec where(any, any) :: [{tuple, [tuple], [any]}]
   def where(condition, output \\ :entry),
-    do: raw({:andalso, unexpired_map_clauses(), condition}, output)
+    do: raw({:andalso, unexpired_clause(), condition}, output)
 
   ###############
   # Private API #
   ###############
-  # Recursively replaces all entry tags in a map_clauses value.
+  # Recursively replaces all entry tags in a clause value.
   #
   # This allows the use of entry fields, such as `:key` as references in
-  # query map_clausess (even if ETS doesn't). The fields will be mapped to the
-  # index equivalent and returned in a sanitized map_clauses value.
+  # query clauses (even if ETS doesn't). The fields will be mapped to the
+  # index equivalent and returned in a sanitized clause value.
   defp map_clauses(tpl) when is_tuple(tpl) do
     tpl
     |> Tuple.to_list()
@@ -111,7 +111,7 @@ defmodule Cachex.Query do
   defp map_clauses(field),
     do: field
 
-  # Sanitizes a returning value map_clauses.
+  # Sanitizes a returning value clause.
   #
   # This will just wrap any non-single element Tuples being returned as
   # this is required in order to provide valid return formats.
